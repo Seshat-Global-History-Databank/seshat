ba21ad3d7d7ff69ca99e2e82a97f969f3eefd6b1
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-01-08 10:55:37 +0000

    Refactor shape variable assignments to use arrays for settlement, religious, military, and administrative levels

--- seshat/apps/core/views.py
+++ seshat/apps/core/views.py
@@ -4371,14 +4371,10 @@ def assign_categorical_variables_to_shapes(shapes, variables):
         shape['alternate_religion_family_dict'] = {}
         shape['alternate_religion'] = []
         shape['alternate_religion_dict'] = {}
-        shape['settlement_hierarchy_from'] = 0
-        shape['settlement_hierarchy_to'] = 0
-        shape['religious_level_from'] = 0
-        shape['religious_level_to'] = 0
-        shape['military_level_from'] = 0
-        shape['military_level_to'] = 0
-        shape['administrative_level_from'] = 0
-        shape['administrative_level_to'] = 0
+        shape['settlement_hierarchy'] = []
+        shape['religious_level'] = []
+        shape['military_level'] = []
+        shape['administrative_level'] = []
         if shape['seshat_id'] != 'none':  # Skip shapes with no seshat_id
             polity = polities.get(shape['seshat_id'])
             if polity:
@@ -4395,20 +4391,20 @@ def assign_categorical_variables_to_shapes(shapes, variables):
                 shape['alternate_religion'].extend([ar.alternate_religion for ar in alternate_religions.get(polity.id, [])])
                 settlement_hierarchy = Settlement_hierarchy.objects.filter(polity_id=polity.id)
                 if settlement_hierarchy:
-                    shape['settlement_hierarchy_from'] = settlement_hierarchy[0].settlement_hierarchy_from
-                    shape['settlement_hierarchy_to'] = settlement_hierarchy[0].settlement_hierarchy_to
+                    shape['settlement_hierarchy'].append(settlement_hierarchy[0].settlement_hierarchy_from)
+                    shape['settlement_hierarchy'].append(settlement_hierarchy[0].settlement_hierarchy_to)
                 religious_level = Religious_level.objects.filter(polity_id=polity.id)
                 if religious_level:
-                    shape['religious_level_from'] = religious_level[0].religious_level_from
-                    shape['religious_level_to'] = religious_level[0].religious_level_to
+                    shape['religious_level'].append(religious_level[0].religious_level_from)
+                    shape['religious_level'].append(religious_level[0].religious_level_to)
                 military_level = Military_level.objects.filter(polity_id=polity.id)
                 if military_level:
-                    shape['military_level_from'] = military_level[0].military_level_from
-                    shape['military_level_to'] = military_level[0].military_level_to
+                    shape['military_level'].append(military_level[0].military_level_from)
+                    shape['military_level'].append(military_level[0].military_level_to)
                 administrative_level = Administrative_level.objects.filter(polity_id=polity.id)
                 if administrative_level:
-                    shape['administrative_level_from'] = administrative_level[0].administrative_level_from
-                    shape['administrative_level_to'] = administrative_level[0].administrative_level_to
+                    shape['administrative_level'].append(administrative_level[0].administrative_level_from)
+                    shape['administrative_level'].append(administrative_level[0].administrative_level_to)
 
                 # Get the years for the variables which have years for the polity
                 shape['linguistic_family_dict'].update({lf.linguistic_family: [lf.year_from, lf.year_to] for lf in linguistic_families.get(polity.id, [])})
