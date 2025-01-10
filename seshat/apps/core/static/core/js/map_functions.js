a5fda5a50bf159150bf059b50ccd499730e1d2c1
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-01-10 11:40:52 +0000

    Add support for multiple polity links in legend for improved navigation

--- seshat/apps/core/static/core/js/map_functions.js
+++ seshat/apps/core/static/core/js/map_functions.js
@@ -363,6 +363,17 @@ function updateLegend() {
             if (shape.seshat_id in seshat_id_page_id) {
                 var polityId = seshat_id_page_id[shape.seshat_id]['id'];
                 shape_name_col_dict['link'] = '<a href="/core/polity/' + polityId + '" target="_blank">' + shape.name + '</a>';
+            } else if (shape.seshat_id.includes(';')) {
+                var polityIds = shape.seshat_id.split(';');
+                var polityLinks = [];
+                polityIds.forEach(function (polityId) {
+                    if (polityId in seshat_id_page_id) {
+                        polityLinks.push('<a href="/core/polity/' + seshat_id_page_id
+                            [polityId]['id'] + '" target="_blank">' + polityId + '</a>');
+                    }
+                }
+                );
+                shape_name_col_dict['link'] = shape.name + '; ' + polityLinks.join('; ');
             } else {
                 shape_name_col_dict['link'] = shape.name;
             }
