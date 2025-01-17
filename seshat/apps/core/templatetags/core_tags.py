300f978be355c6d821ace07074c5de64b4e6bb54
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-01-17 11:01:06 +0000

    Update shape year attributes in polity map for suprapolity relations

--- seshat/apps/core/templatetags/core_tags.py
+++ seshat/apps/core/templatetags/core_tags.py
@@ -65,6 +65,10 @@ def polity_map(pk, test=False):
         for spr in suprapolity_relations:
             polity = relation_polity_info[spr['other_polity_id']]
             spr['shapes'] = get_polity_shape_content(seshat_id=polity['new_name'])['shapes']
+            # Replace the start_year and end_year of each shape with the year_from and to_year of the relation
+            for shape in spr['shapes']:
+                shape['start_year'] = spr['year_from']
+                shape['end_year'] = spr['year_to']
             if polity['new_name'] not in relation_seshat_ids:
                 relation_seshat_ids.append(polity['new_name'])
         
