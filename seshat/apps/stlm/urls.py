02dd54c3d89db6cb45658332cf2f7456e48145fa
Majid Benam <benam.mjd@gmail.com>
2025-10-30 18:24:39 +0100

    added allen data download to the settlments data

--- seshat/apps/stlm/urls.py
+++ seshat/apps/stlm/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('download_csv_settlement_population/', views.settlement_population_download,name='download_csv_settlement_population'),
+    path('download_csv_settlement_allen_data/', views.generate_stlm_csv_response,name='download_csv_settlement_allen_data'),
 ]
--- seshat/apps/stlm/views.py
+++ seshat/apps/stlm/views.py
@@ -9,6 +9,40 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
 
 from .models import Settlement_population
+from django.apps import apps
+
+
+@permission_required('core.add_capital')
+def generate_stlm_csv_response(request):
+    # Fetch all models in the "sc" app
+    app_name = 'stlm'
+    app_models = apps.get_app_config(app_name).get_models()
+
+    # Create a response object with CSV content type
+    response = HttpResponse(content_type='text/csv')
+    current_datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_name = f"settlement_allen_data_{current_datetime}.csv"
+    response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+
+    # Create a CSV writer
+    writer = csv.writer(response, delimiter='|')
+    
+    # Write the headers
+    writer.writerow(['variable_name',  'settlement_name', 'settlement_alternative_names', 'current_country', 'latitude' , 'longitude', 'year_from', 'year_to', 'value', 'certainty', 'scientific_resource' ])
+    
+    # Iterate over each model and filter by subsection
+    for model in app_models:
+        if model.__name__ in ["Number_of_ziggurats", "Number_of_palaces", "Number_of_temples", "Defensive_wall", "Tablet", "Seal_indicator"]:
+            items = model.objects.exclude(settlement_id__isnull=True)
+            for obj in items:
+                if obj.settlement:
+                    writer.writerow([obj.name,
+                            obj.settlement.name, obj.settlement.alternative_names, obj.settlement.current_country_obj.name, obj.settlement.latitude, obj.settlement.longitude,  obj.year_from, obj.year_to, obj.show_value(), obj.tag, obj.general_ref.title])
+                else:
+                    writer.writerow([obj.name,
+                             "no_name", "no_name", "no_name", obj.settlement.latitude, obj.settlement.longitude,  obj.year_from, obj.year_to, obj.show_value(), obj.tag, obj.general_ref.title])
+
+    return response
 
 
 # Create your views here.
